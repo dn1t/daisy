afde35ef611b69e681259033f449814cfcacb911
dn1t <me@tica.fun>
2026-07-23 15:00:51 +0900

    tailwind-config: added @toolwind/corner-shape

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -2,7 +2,7 @@ import type { ComponentProps } from "solid-js";
 import { tv } from "tailwind-variants";
 
 const button = tv({
-  base: "cursor-pointer rounded-[10px] supports-[corner-shape:squircle]:rounded-full bg-zinc-900 px-4.5 py-2.75 font-semibold text-[15px] text-white leading-none [corner-shape:squircle]",
+  base: "cursor-pointer rounded-[10px] supports-corner-shape:rounded-full bg-zinc-900 px-4.5 py-2.75 font-semibold text-[15px] text-white leading-none corner-squircle",
   variants: {},
 });
 

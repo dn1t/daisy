ccdef0a99508a7bc234edd67a33c0b297ae4ed1e
dn1t <me@tica.fun>
2026-07-23 22:38:24 +0900

    web: added wrangler

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,5 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), solidStart(), nitro()],
+  plugins: [
+    tailwindcss(),
+    solidStart(),
+    nitro({
+      preset: "cloudflare-pages",
+      cloudflare: {
+        wrangler: {
+          name: "daisy",
+        },
+      },
+      rollupConfig: {
+        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+      },
+    }),
+  ],
 });

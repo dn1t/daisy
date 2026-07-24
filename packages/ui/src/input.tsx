8257ff9dffce4604a7fb646123c0a39be36d542f
dn1t <me@tica.fun>
2026-07-25 01:15:08 +0900

    ui: added ring to focused Input

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends ComponentProps<"input"> {
 
 export function Input(props: InputProps) {
   return (
-    <label class="corner-squircle group/input flex cursor-text flex-col rounded-xl border border-zinc-200 bg-zinc-100 supports-corner-shape:rounded-3xl dark:border-zinc-800 dark:bg-zinc-900">
+    <label class="corner-squircle group/input flex w-full cursor-text flex-col rounded-xl border border-zinc-200 bg-zinc-100 focus-within:border-brand-500 focus-within:ring-2 focus-within:ring-brand-200 supports-corner-shape:rounded-3xl dark:border-zinc-800 dark:bg-zinc-900 dark:focus-within:ring-brand-800">
       {props.label && (
         <span class="select-none px-3.25 pt-2.75 font-medium text-xs text-zinc-500 leading-none group-focus-within/input:text-zinc-800 dark:group-focus-within/input:text-zinc-200">
           {props.label}

c665b6a5fd45c7ac561d419ad9bdba7389850007
dn1t <me@tica.fun>
2026-07-25 22:47:54 +0900

    web: updated wrangler config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,22 +1,23 @@
+import { config } from "dotenv";
 import { defineConfig } from "vite";
 
 import { solidStart } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 
+config({ path: "../../.env", quiet: true });
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     solidStart(),
     nitro({
-      preset: "cloudflare-pages",
-      cloudflare: {
-        wrangler: {
-          name: "daisy",
+      preset: "cloudflare-module",
+      storage: {
+        kv: {
+          driver: "cloudflare-kv-binding",
+          binding: "daisy",
         },
-      },
-      rollupConfig: {
-        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
       },
     }),
   ],

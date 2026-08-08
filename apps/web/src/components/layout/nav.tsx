d8c6721bb81e0171d766f8072e5d94595f2c1040
dn1t <me@tica.fun>
2026-08-08 14:02:27 +0900

    web: added profile image on nav when logon

--- apps/web/src/components/layout/nav.tsx
+++ apps/web/src/components/layout/nav.tsx
@@ -4,6 +4,7 @@ import type { User } from "@daisy/auth/session";
 import { Button, Logo, Tabs, useModal, type LinkTab } from "@daisy/ui";
 import { useEffect, useMemo } from "react";
 import { Link, useRouter } from "waku";
+import { UserIcon } from "@phosphor-icons/react";
 import { LoginModal } from "./login-modal";
 
 const tabs: LinkTab[] = [
@@ -34,10 +35,20 @@ export function Nav({ user, verifyPostId }: { user: User | null; verifyPostId: s
           Daisy
         </Link>
         <Tabs tabs={tabs} selected={selected} Link={Link} />
-        <Button className="ml-auto" onClick={() => setOpen(true)}>
-          시작하기
-        </Button>
-        <LoginModal {...modalProps} verifyPostId={verifyPostId} />
+        {user && (
+          <button type="button" className="ml-auto h-9.5 w-9.5">
+            {user.image && <img src={user.image} alt="프로필 사진" />}
+            {!user.image && <UserIcon weight="fill" />}
+          </button>
+        )}
+        {!user && (
+          <>
+            <Button className="ml-auto" onClick={() => setOpen(true)}>
+              시작하기
+            </Button>
+            <LoginModal {...modalProps} verifyPostId={verifyPostId} />
+          </>
+        )}
       </div>
     </nav>
   );
